b6d911e6043f3989db538d8d31e9956239d149d9
NelviCastellanos <nelvicastellanos@wolfsellers.com>
2025-12-17 15:29:30 -0600

    correccion de errores

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,23 @@
+/*
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
 ReactDOM.render(<App />, document.getElementById('app'))
+*/
+
+
+import React from 'react'
+import { createRoot } from 'react-dom/client'  // ← Import correcto
+import App from './App'
+import './styles.css'
+
+const container = document.getElementById('app')
+const root = createRoot(container)              // ← Crea la raíz React
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
